54406dbe0cd7c45cfb33248e748224fcdccf8637
Jacob Carlstrom <jacobcarlstrom8@gmail.com>
2023-08-25 08:04:36 -0400

    Added print statement

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
-
+		System.out.println("Jake is working locally on his branch");
 	}
 
 }
